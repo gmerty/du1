e525b00982f4dc0b177bbf256210b241594172ee
dns <ich.ast.08@gmail.com>
2018-04-08 21:40:28 +0200

    nastaveni HomeControlleru

--- du1/src/main/java/com/github/gmerty/du1/ui/App.java
+++ du1/src/main/java/com/github/gmerty/du1/ui/App.java
@@ -1,9 +1,11 @@
 package com.github.gmerty.du1.ui;
 
-import com.github.gmerty.du1.ui.HomeController;
+import com.github.gmerty.du1.logika.*;
+import com.github.gmerty.du1.ui.*;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -12,18 +14,26 @@ import javafx.stage.Stage;
  */
 public class App extends javafx.application.Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		if (args.length == 0) {
+            launch(args);
+        } else 
+                System.out.println("Neplatný parametr");
+	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass()
 		          .getResource("du2scene.fxml"));
 		Parent root = loader.load();
 
-		HomeController controller = loader.getController();		
+		HomeController controller = loader.getController();
+		controller.inicializuj(new Hra());
+		
+		primaryStage.setTitle("du1");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
 	}
 }
--- du1/src/main/java/com/github/gmerty/du1/ui/HomeController.java
+++ du1/src/main/java/com/github/gmerty/du1/ui/HomeController.java
@@ -2,12 +2,19 @@ package com.github.gmerty.du1.ui;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Random;
 
 import com.github.gmerty.du1.logika.*;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class HomeController implements Observer{
 	
 	private Hra hra;
+	Random rand = new Random();
+	@FXML private Label vypis;
+	String otazka;
 
 	public void inicializuj(Hra hra) {
 		this.hra = hra;
@@ -17,6 +24,44 @@ public class HomeController implements Observer{
 	public void update(Observable arg0, Object arg1) {
 		
 		
+	}
+	
+	public void bHadej() {
+		int ran = rand.nextInt(100) % 7;
+		for (Integer num : hra.getSeznamSlov().keySet()) {
+			if (ran == num) {
+				vypis.setText(hra.getSeznamSlov().get(num).getAng());
+				otazka = hra.getSeznamSlov().get(num).getCz();
+			}
+		}
+		
+	}
+	
+	public void bBear() {
+		if (otazka.equals(hra.getSeznamSlov().get(1).getAng())) {
+			
+		}
+		
+	}
+	
+	public void bFox() {
+		
+	}
+	
+	public void bRabbit() {
+		
+	}
+
+	public void bSquirrel() {
+	
+	}
+
+	public void bWolf() {
+	
+	}
+
+	public void bHedgehog() {
+	
 	}
 
 }
